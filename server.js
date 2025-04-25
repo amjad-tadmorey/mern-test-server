bff75231ab89673e5c29897c6b54c2fa59c7cbf6
amjad <amjadtadmorey@gmail.com>
2025-04-25 19:34:36 +0300

    .

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 const app = require('./app')
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Error connecting to MongoDB:', err));
 
 const port = process.env.PORT
 const server = app.listen(port, () => {
