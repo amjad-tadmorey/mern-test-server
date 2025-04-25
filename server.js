205821981085ecb0e77b0d557f89cfa9b324be5e
amjad <amjadtadmorey@gmail.com>
2025-04-25 17:13:32 +0300

    .

--- server.js
+++ server.js
@@ -3,5 +3,11 @@ const app = require('./app')
 
 const port = process.env.PORT
 const server = app.listen(port, () => {
-    console.log(`Runing on port tesssssssssssssssssst ${port}...${process.env.NODE_ENV}`);
+    console.log(`Runing on port ${port}...${process.env.NODE_ENV}`);
 })
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Server closed gracefully');
+    });
+});
