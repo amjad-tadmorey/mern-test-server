49de4c22684db7ca5939259c5654eccf330d274c
amjad <amjadtadmorey@gmail.com>
2025-04-25 20:15:13 +0300

    .

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -4,7 +4,6 @@ const catchAsync = require('../utils/catchAsync')
 exports.getAllOrders = catchAsync(async (req, res, next) => {
 
     const orders = await Order.find();
-
     // SEND RESPONSE
     res.status(200).json({
         status: 'success',
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log('Error connecting to MongoDB:', err));
 
