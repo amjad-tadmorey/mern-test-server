dadbda4e6ea0151b48284331d4f23ec89d325bfe
amjad <amjadtadmorey@gmail.com>
2025-04-25 16:53:55 +0300

    .

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app')
 
 const port = process.env.PORT
 const server = app.listen(port, () => {
-    console.log(`Runing on port ${port}...${process.env.NODE_ENV}`);
+    console.log(`Runing on port tesssssssssssssssssst ${port}...${process.env.NODE_ENV}`);
 })
